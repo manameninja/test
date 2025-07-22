fbe247cf5e6cb88c3bd8957fd1a35a9a30e05407
Daniil Pavlenko <64921608+manameninja@users.noreply.github.com>
2025-07-22 10:00:01 +0300

    add project files

---
+++ Insta/Insta/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by aleksey.shchedrin on 08.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Устанавливаем цвет фона белым
+        view.backgroundColor = .white
+        
+        // Создаем UILabel
+        let label = UILabel()
+        label.text = "Привет"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Добавляем UILabel на экран
+        view.addSubview(label)
+        
+        // Настраиваем constraints для центрирования лейбла
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
